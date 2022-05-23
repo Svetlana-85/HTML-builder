742202f826e7fd4deba7ee72fdec79dbbd7134c1
Svetlana <svetlanka-85@yandex.ru>
2022-05-23 23:40:48 +0500

    feat: added task solution 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,37 @@
+const path = require('path');
+const fs = require('fs');
+const fsPromises = fs.promises;
 
+const pathFolderIn = path.join(__dirname,'files-copy');
+const pathFolderOut = path.join(__dirname, 'files');
+
+fs.stat(pathFolderIn, function(err) {
+  if (!err) {
+    fs.readdir(pathFolderIn, {withFileTypes: true, encoding: 'utf8'}, (err) => {
+      if (err) throw err;
+      fs.rm(pathFolderIn, {recursive: true}, () => {
+        copyFolder();
+      });
+    });
+  } 
+  else {
+    copyFolder();
+  }
+});
+
+function copyFolder(){
+  fsPromises.mkdir(pathFolderIn, { recursive:true}).then(function() {
+    fs.readdir(pathFolderOut, {withFileTypes: true, encoding: 'utf8'}, (err, files) => {
+      if (err) throw err;
+      files.forEach(function(item){
+        if(item.isFile()){
+          const fileNameOut = path.join(pathFolderOut,item.name);
+          const fileNameIn = path.join(pathFolderIn,item.name);
+          fs.copyFile(fileNameOut, fileNameIn, () => {});
+        }
+      });
+    });
+  }).catch(function() {
+    console.log('failed to create directory');
+  });
+}
