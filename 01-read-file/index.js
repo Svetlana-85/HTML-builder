6336a768ac65569f05698bb17a66ec6261916071
Svetlana <svetlanka-85@yandex.ru>
2022-05-23 23:37:37 +0500

    feat: added task solution 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,9 @@
+const path = require('path');
+const fs = require('fs');
+
+const stream = new fs.ReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+let data = '';
+
+stream.on('data', chunk => data += chunk);
+stream.on('close', () => console.log(data));
+stream.on('error', error => console.log('Error', error.message));
